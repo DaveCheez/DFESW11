cbeb2d26003bf15dee8161e83c8cd853ea14a63e
David Barrow <davecheez21@gmail.com>
2022-02-07 14:54:52 +0000

    Completed excercise on datatypes

--- Py/main.py
+++ Py/main.py
@@ -5,6 +5,24 @@ print("Hello")
 x = 101             # integer
 y = 10.5            # float
 name = 'David'      # string
+l_name = 'Barrow'   # string
 f = False           # Boolean
 t = True            # Boolean
 
+print(x + y)               # Calculation
+print(name + ' ' + l_name) # Concatenation
+
+# Type Casting booleans to integers
+print(int(f))
+print(int(t))
+
+# Booleans
+
+name = "Pep Guardogiola"
+age = 3
+bark = True
+tweet = False
+
+print("My pet is called", name, ", He is", age, "years old.")
+print("Statement:", name, "barks.", bark)
+print("Statement:", name, "tweets.", tweet)
