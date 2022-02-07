6a42b4ae675522ea8099787715518b2b6cc78a47
David Barrow <davecheez21@gmail.com>
2022-02-07 14:55:21 +0000

    updated excercise on datatypes

--- Py/main.py
+++ Py/main.py
@@ -17,7 +17,6 @@ print(int(f))
 print(int(t))
 
 # Booleans
-
 name = "Pep Guardogiola"
 age = 3
 bark = True
@@ -26,3 +25,16 @@ tweet = False
 print("My pet is called", name, ", He is", age, "years old.")
 print("Statement:", name, "barks.", bark)
 print("Statement:", name, "tweets.", tweet)
+
+
+# Numbers & Floats
+number1 = input("Enter whole number: ")
+number2 = input("Enter decimal number: ")
+
+integer_number = int(number1)
+float_number = float(number2)
+round_number = int(round(float_number))
+
+print(number1)
+print(number2)
+print(round_number)
