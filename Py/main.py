554c1beedc223af0149f29dbc8e94439380e0f7b
David Barrow <davecheez21@gmail.com>
2022-02-07 13:03:37 +0000

    added variable assignment

--- Py/main.py
+++ Py/main.py
@@ -1,1 +1,10 @@
+# Print Function
 print("Hello")
+
+# Variable Assignment
+x = 101             # integer
+y = 10.5            # float
+name = 'David'      # string
+f = False           # Boolean
+t = True            # Boolean
+
