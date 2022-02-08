12c9f6a6ad25d15bdbaa0ba59ea2d0128f8fc391
David Barrow <davecheez21@gmail.com>
2022-02-08 10:12:14 +0000

    lists and dicts added

--- Py/main.py
+++ Py/main.py
@@ -38,3 +38,33 @@ round_number = int(round(float_number))
 print(number1)
 print(number2)
 print(round_number)
+
+# Using .items() method generate a list of tuples consisted of each key value pair.
+dict = {"son's name": "Lucas", "son's eyes": "green", "son's height": 32, "son's weight": 25}
+ans_1 = dict.items()
+
+print(ans_1)
+
+# Using .get() method print the value of "son's eyes".
+dict = {"son's name": "Lucas", "son's eye color": "green", "son's height": 32, "son's weight": 25}
+ans_1 = dict.get("son's eye color")
+
+print(ans_1)
+
+# Assign the first element of the list to answer_1 on line 2
+lst = [11, 100, 99, 1000, 999]
+answer_1 = lst[0]
+
+print(answer_1)
+
+# Print the last element of the list through variable answer_1.
+lst=[11, 100, 101, 999, 1001]
+answer_1 = lst[-1]
+
+print(answer_1)
+
+# Assign the first element of the list to answer_1 
+lst=[11, 100, 99, 1000, 999]
+answer_1 = lst[0]
+
+print(answer_1)
