3c98be6a4d7e3f96b413be6daad3b9abda953410
David Barrow <davecheez21@gmail.com>
2022-02-08 12:06:15 +0000

    updated logic exercises

---
+++ Py/logic.py
@@ -0,0 +1,29 @@
+# Logic Exercises
+
+"""
+Create a new Python file and write a program that does the following:
+    Asks for an input from the user as a mark.
+    If the mark is greater that 85 print "Distinction"
+    If the mark is between 65 and 85 print "Pass"
+    Anything else print "Fail"
+"""
+mark = int(input("Please enter your mark:\n"))
+
+if mark > 85:
+    print("Distinction")
+elif mark >= 65 and mark <= 85:
+    print("Pass")
+else:
+    print("Fail")
+
+
+"""Try to do this both with and without elif statements."""
+
+mark2 = int(input("Please enter your second mark:\n"))
+
+if mark2 > 85:
+    print("Distinction")
+    if mark2 >= 65:
+        print("Pass")
+    else: 
+        print("Fail")
