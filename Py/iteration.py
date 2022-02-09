9bd7c8ab0498dba78ea06cf236a10acee89aeb2c
David Barrow <davecheez21@gmail.com>
2022-02-09 09:42:49 +0000

    updated

--- Py/iteration.py
+++ Py/iteration.py
@@ -60,3 +60,18 @@ elif temp == 'f':
 else:
     print("Incorrect value for temperature")
 
+
+
+"""
+4. Write a Python program to construct the following pattern, using a nested for loop.
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+"""
